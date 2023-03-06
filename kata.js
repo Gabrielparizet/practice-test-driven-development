b6377baf64eb6b8ab671f4318c9362f35d50b5ac
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-06 17:56:34 +0100

    (feat) writing unitary tests and function for negative numbers

--- kata.js
+++ kata.js
@@ -4,6 +4,14 @@ const add = (myStr) => {
     const delimiter = findDelimiter(myStr);
     let numbersToAdd = myStr.split('\n').join(delimiter).split(delimiter);
     numbersToAdd = convertArrayOfStringToArrayOfIntegers(numbersToAdd);
+    let hasNegatives = hasNegativeNumber(numbersToAdd);
+    if (hasNegatives.length > 0){
+        let result = "";
+        for (i in hasNegatives){
+            result += " " + hasNegatives[i];
+        }
+        return "Error, negative numbers are not authorized:" + result + ".";
+    }
     if (myStr === ""){
         return 0;
     } else if (numbersToAdd.length === 1) {
@@ -34,9 +42,17 @@ const convertArrayOfStringToArrayOfIntegers = (myArr) => {
     return result;
 };
 
-// convertArrayOfStringToArrayOfIntegers(["1", "2", "3", "abc"]);
-convertArrayOfStringToArrayOfIntegers(['//', '', '1', '2']);
-add("//;\n1;2");
+const hasNegativeNumber = (myArr) => {
+    let result = [];
+    for (i in myArr){
+        if (myArr[i] < 0){
+            result.push(myArr[i].toString());
+        };
+    };
+    return result
+};
 
+// add('//;\n1;2');
+console.log(add('//;\n1;-2'));
 
-module.exports = {add, findDelimiter, convertArrayOfStringToArrayOfIntegers};
+module.exports = {add, findDelimiter, convertArrayOfStringToArrayOfIntegers, hasNegativeNumber};
--- kata.test.js
+++ kata.test.js
@@ -1,49 +1,4 @@
-const {add, findDelimiter, convertArrayOfStringToArrayOfIntegers} = require('./kata');
-
-
-describe ("Test findDelimiter() function", () => {
-
-    it("If the string given to findDelimiter as argument contains a first line with a delimiter, findDelimiter should return that delimiter as a string." , () => {
-        // Given
-            const myStr = "//;\n1;2";
-        // When
-            const result = findDelimiter(myStr);
-        // Then
-            expect(result).toEqual(";");
-    });
-
-    it("If the string given to findDelimiter as argument does not contain a first line with a delimiter, findDelimiter should a comma as a string." , () => {
-        // Given
-            const myStr = "1\n2,3";
-            const myStr2 = "1,2,3";
-        // When
-            const result = findDelimiter(myStr);
-            const result2 = findDelimiter(myStr2);
-        // Then
-            expect(result).toEqual(",");
-            expect(result2).toEqual(",");
-    });
-
-});
-
-describe("Test convertArrayOfStringToArrayOfIntegers() function", () => {
-    it("Being given an array containing different types of data as strings, convertArrayOfStringToArrayOfInters() should return an array only containing integers.", () => {
-        // Given
-            const myArr = ["//", "abc", "1", "//", "3", "4"];
-            const myArr2 = ["1"];
-            const myArr3 = ["abc"];
-        // When
-            const result = convertArrayOfStringToArrayOfIntegers(myArr);
-            const result2 = convertArrayOfStringToArrayOfIntegers(myArr2);
-            const result3 = convertArrayOfStringToArrayOfIntegers(myArr3);
-        // Then
-            expect(result).toEqual([1, 3, 4]);
-            expect(result2).toEqual([1]);
-            expect(result3).toEqual([]);
-    });
-});
-
-
+const {add, findDelimiter, convertArrayOfStringToArrayOfIntegers, hasNegativeNumber} = require('./kata');
 
 describe("Test add() function" , () => {
 
@@ -104,11 +59,83 @@ describe("Test add() function" , () => {
 
     it("Being give a string with different delimiters as arguement before a \n separator, add() should return the sum of the numbers delimited by that delimiter as an integer", () => {
         // Given
-            const myStr = "//;\n1;2"
+            const myStr = "//;\n1;2";
         // When
             const result = add(myStr);
         // Then
             expect(result).toEqual(3);
+    });
+
+    it("Being given negative numbers in the string given as parameters, add() should return an error message containing those negative numbers." , () => {
+        // Given
+            const myStr = "//;\n1;2;-3";
+        // When
+            const result = add(myStr);
+        // Then
+            expect(result).toEqual("Error, negative numbers are not authorized: -3.")
+    });
+
+});
+
+describe ("Test findDelimiter() function", () => {
+
+    it("If the string given to findDelimiter as argument contains a first line with a delimiter, findDelimiter should return that delimiter as a string." , () => {
+        // Given
+            const myStr = "//;\n1;2";
+        // When
+            const result = findDelimiter(myStr);
+        // Then
+            expect(result).toEqual(";");
+    });
+
+    it("If the string given to findDelimiter as argument does not contain a first line with a delimiter, findDelimiter should a comma as a string." , () => {
+        // Given
+            const myStr = "1\n2,3";
+            const myStr2 = "1,2,3";
+        // When
+            const result = findDelimiter(myStr);
+            const result2 = findDelimiter(myStr2);
+        // Then
+            expect(result).toEqual(",");
+            expect(result2).toEqual(",");
+    });
+
+});
+
+describe("Test convertArrayOfStringToArrayOfIntegers() function", () => {
+
+    it("Being given an array containing different types of data as strings, convertArrayOfStringToArrayOfInters() should return an array only containing integers.", () => {
+        // Given
+            const myArr = ["//", "abc", "1", "//", "3", "4"];
+            const myArr2 = ["1"];
+            const myArr3 = ["abc"];
+        // When
+            const result = convertArrayOfStringToArrayOfIntegers(myArr);
+            const result2 = convertArrayOfStringToArrayOfIntegers(myArr2);
+            const result3 = convertArrayOfStringToArrayOfIntegers(myArr3);
+        // Then
+            expect(result).toEqual([1, 3, 4]);
+            expect(result2).toEqual([1]);
+            expect(result3).toEqual([]);
+    });
+
+});
+
+describe("Test hasNegativeNumbers() function", () => {
+
+    it("Being given an array of integers, if some of these integers are negative numbers, hasNegativeNumbers() should return these negative numbers as an array of negative numbers as strings", () => {
+        // Given
+            const myArr = [-2, 3, 6, -5];
+            const myArr2 = [3, 5, 7, 8];
+            const myArr3 = [-3];
+        // When
+            const result = hasNegativeNumber(myArr);
+            const result2 = hasNegativeNumber(myArr2);
+            const result3 = hasNegativeNumber(myArr3);
+        // Then
+            expect(result).toEqual(["-2", "-5"]);
+            expect(result2).toEqual([]);
+            expect(result3).toEqual(["-3"]);
     });
 
 });
