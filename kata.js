8e5e5b6f1dd3515c0d187390408ef8a0a5a23f58
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-06 16:38:25 +0100

    (feat) Writing tests for findDelimiter and convertArrayOfStringToArrayOfIntegers

--- kata.js
+++ kata.js
@@ -1,19 +1,42 @@
 // Create a string calculator
 
 const add = (myStr) => {
-    const numbersToAdd = myStr.split(",");
+    const delimiter = findDelimiter(myStr);
+    let numbersToAdd = myStr.split('\n').join(delimiter).split(delimiter);
+    numbersToAdd = convertArrayOfStringToArrayOfIntegers(numbersToAdd);
     if (myStr === ""){
         return 0;
     } else if (numbersToAdd.length === 1) {
         return parseInt(myStr);
     } else {
         let result = numbersToAdd.reduce((acc, curr) => 
-            parseInt(acc) + parseInt(curr), 0
+            acc + curr, 0
         );
         return result;
+    };
+};
+
+const findDelimiter = (myStr) => {
+    if (myStr[0] === "/" && myStr[1] === "/"){
+        return myStr[2];
+    } else {
+        return ",";
+    };
+};
+
+const convertArrayOfStringToArrayOfIntegers = (myArr) => {
+    let result = [];
+    for (i in myArr){
+        if (!isNaN(myArr[i]) && myArr[i] != ""){
+            result.push(parseInt(myArr[i]));
+        }
     }
-}
+    return result;
+};
 
-console.log(add("1, 2"));
+// convertArrayOfStringToArrayOfIntegers(["1", "2", "3", "abc"]);
+convertArrayOfStringToArrayOfIntegers(['//', '', '1', '2']);
+add("//;\n1;2");
 
-module.exports = {add};
+
+module.exports = {add, findDelimiter, convertArrayOfStringToArrayOfIntegers};
--- kata.test.js
+++ kata.test.js
@@ -1,8 +1,53 @@
-const {add} = require('./kata');
+const {add, findDelimiter, convertArrayOfStringToArrayOfIntegers} = require('./kata');
 
-describe("Test add function" , () => {
 
-    it("With an empty string, add() should return 0", () => {
+describe ("Test findDelimiter() function", () => {
+
+    it("If the string given to findDelimiter as argument contains a first line with a delimiter, findDelimiter should return that delimiter as a string." , () => {
+        // Given
+            const myStr = "//;\n1;2";
+        // When
+            const result = findDelimiter(myStr);
+        // Then
+            expect(result).toEqual(";");
+    });
+
+    it("If the string given to findDelimiter as argument does not contain a first line with a delimiter, findDelimiter should a comma as a string." , () => {
+        // Given
+            const myStr = "1\n2,3";
+            const myStr2 = "1,2,3";
+        // When
+            const result = findDelimiter(myStr);
+            const result2 = findDelimiter(myStr2);
+        // Then
+            expect(result).toEqual(",");
+            expect(result2).toEqual(",");
+    });
+
+});
+
+describe("Test convertArrayOfStringToArrayOfIntegers() function", () => {
+    it("Being given an array containing different types of data as strings, convertArrayOfStringToArrayOfInters() should return an array only containing integers.", () => {
+        // Given
+            const myArr = ["//", "abc", "1", "//", "3", "4"];
+            const myArr2 = ["1"];
+            const myArr3 = ["abc"];
+        // When
+            const result = convertArrayOfStringToArrayOfIntegers(myArr);
+            const result2 = convertArrayOfStringToArrayOfIntegers(myArr2);
+            const result3 = convertArrayOfStringToArrayOfIntegers(myArr3);
+        // Then
+            expect(result).toEqual([1, 3, 4]);
+            expect(result2).toEqual([1]);
+            expect(result3).toEqual([]);
+    });
+});
+
+
+
+describe("Test add() function" , () => {
+
+    it("With an empty string, add() should return 0.", () => {
         // Given
             const myStr = "";
         // When
@@ -11,7 +56,7 @@ describe("Test add function" , () => {
             expect(result).toEqual(0);
     });
     
-    it("With a string containing one number, add() should return that number as an integer", () => {
+    it("With a string containing one number, add() should return that number as an integer?", () => {
         // Given
             const myStr = "1";
         // When
@@ -20,7 +65,7 @@ describe("Test add function" , () => {
             expect(result).toStrictEqual(1);
     });
 
-    it("Given a string as argument, add() should always return an integer", () => {
+    it("Given a string as argument, add() should always return an integer.", () => {
         // Given
             const myStr = "";
         // When
@@ -30,14 +75,41 @@ describe("Test add function" , () => {
             expect(typeof(result)).toEqual("number");
     });
 
-    it("Being given a string containing two numbers as argument, add() should return the sum of these numbers as an integer", () => {
+    it("Being given a string containing two numbers as argument, add() should return the sum of these numbers as an integer.", () => {
         // Given
-            const myStr = "1, 2";
+            const myStr = "1,2";
         // When
             const result = add(myStr);
         // Then
             expect(result).toEqual(3);
-    })
+    });
 
+    it("Being given a string containing more than two numbers as argument, add() should return the sum of these numbers as an integer.", () => {
+        // Given
+            const myStr = "1,2,3,4";
+        // When
+            const result = add(myStr);
+        // Then
+            expect(result).toEqual(10);
+    });
+
+    it("Being given a string containing numbers separated by commas or \n, add() should return the sum of the numbers as an integer.", () => {
+        // Given
+            const myStr = "1\n2,3";
+        // When
+            const result = add(myStr);
+        // Then
+            expect(result).toEqual(6);
+    });
+
+    it("Being give a string with different delimiters as arguement before a \n separator, add() should return the sum of the numbers delimited by that delimiter as an integer", () => {
+        // Given
+            const myStr = "//;\n1;2"
+        // When
+            const result = add(myStr);
+        // Then
+            expect(result).toEqual(3);
+    });
 
 });
+
